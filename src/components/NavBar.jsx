cef4ff7e7ebc6cf0c2d67b2063b779639be5a7ac
cshepscorp <sheppard.christy@gmail.com>
2022-03-12 23:17:34 -0500

    nav mobile working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -61,7 +61,7 @@ function Navbar() {
                   </Link>
 
                   <Link
-                    to="/portfolio"
+                    to="/contact"
                     className={
                       pathMatchRoute('/contact')
                         ? 'btn btn-ghost navbarListItemNameActive'
@@ -157,7 +157,7 @@ function Navbar() {
           )}
         </Transition>
       </nav>
-      {/* 
+
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
@@ -166,12 +166,12 @@ function Navbar() {
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {/* <!-- Replace with your content --> */}
-      {/* <div className="px-4 py-6 sm:px-0">
+          <div className="px-4 py-6 sm:px-0">
             <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-          </div> */}
-      {/* <!-- /End replace --> */}
-      {/*  </div>
-      </main> */}
+          </div>
+          {/* <!-- /End replace --> */}
+        </div>
+      </main>
     </div>
   );
 }
