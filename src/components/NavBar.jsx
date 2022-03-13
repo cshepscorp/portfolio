02f535e5dccfdc3d7198a518c24ca29988189c6f
cshepscorp <sheppard.christy@gmail.com>
2022-03-12 23:14:15 -0500

    nav mobile working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,70 +1,88 @@
 import React, { useState } from 'react';
 import { Transition } from '@headlessui/react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) {
+      return true;
+    }
+  };
+
   return (
     <div>
       <nav className="bg-gray-800">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
-              <div className="flex-shrink-0">
+              {/* <div className="flex-shrink-0">
                 <img
                   className="h-8 w-8"
                   src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
                   alt="Workflow"
                 />
-              </div>
+              </div> */}
               <div className="hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
-                  <a
-                    href="#"
-                    className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  <Link
+                    to="/"
+                    className={
+                      pathMatchRoute('/')
+                        ? 'btn btn-ghost navbarListItemNameActive'
+                        : 'btn btn-ghost navbarListItemName'
+                    }
                   >
-                    Dashboard
-                  </a>
+                    Home
+                  </Link>
+                  <Link
+                    to="/about"
+                    className={
+                      pathMatchRoute('/about')
+                        ? 'btn btn-ghost navbarListItemNameActive'
+                        : 'btn btn-ghost navbarListItemName'
+                    }
+                  >
+                    About
+                  </Link>
 
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  <Link
+                    to="/portfolio"
+                    className={
+                      pathMatchRoute('/portfolio')
+                        ? 'btn btn-ghost navbarListItemNameActive'
+                        : 'btn btn-ghost navbarListItemName'
+                    }
                   >
-                    Team
-                  </a>
+                    Portfolio
+                  </Link>
 
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  <Link
+                    to="/portfolio"
+                    className={
+                      pathMatchRoute('/contact')
+                        ? 'btn btn-ghost navbarListItemNameActive'
+                        : 'btn btn-ghost navbarListItemName'
+                    }
                   >
-                    Projects
-                  </a>
-
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Calendar
-                  </a>
-
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >
-                    Reports
-                  </a>
+                    Contact
+                  </Link>
                 </div>
               </div>
             </div>
             <div className="-mr-2 flex md:hidden">
               <button
-                onClick={() => setIsOpen(!isOpen)}
+                onClick={() => setNavOpen(!navOpen)}
                 type="button"
                 className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
                 <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
+                {!navOpen ? (
                   <svg
                     className="block h-6 w-6"
                     xmlns="http://www.w3.org/2000/svg"
@@ -103,7 +121,7 @@ function Navbar() {
         </div>
 
         <Transition
-          show={isOpen}
+          show={navOpen}
           enter="transition ease-out duration-100 transform"
           enterFrom="opacity-0 scale-95"
           enterTo="opacity-100 scale-100"
@@ -114,46 +132,32 @@ function Navbar() {
           {(ref) => (
             <div className="md:hidden" id="mobile-menu">
               <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <a
-                  href="#"
+                <Link
+                  to="/about"
                   className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Dashboard
-                </a>
+                  About
+                </Link>
 
-                <a
-                  href="#"
+                <Link
+                  href="/portfolio"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Team
-                </a>
+                  Portfolio
+                </Link>
 
-                <a
-                  href="#"
+                <Link
+                  href="/contact"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  Projects
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Calendar
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Reports
-                </a>
+                  Contact
+                </Link>
               </div>
             </div>
           )}
         </Transition>
       </nav>
-
+      {/* 
       <header className="bg-white shadow">
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
@@ -162,12 +166,12 @@ function Navbar() {
       <main>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           {/* <!-- Replace with your content --> */}
-          <div className="px-4 py-6 sm:px-0">
+      {/* <div className="px-4 py-6 sm:px-0">
             <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
-          </div>
-          {/* <!-- /End replace --> */}
-        </div>
-      </main>
+          </div> */}
+      {/* <!-- /End replace --> */}
+      {/*  </div>
+      </main> */}
     </div>
   );
 }
